feaa959c1aa87c36d22b1f02d8479f725c7efc32
Evan Ferreira <evanjfer@gmail.com>
2024-12-12 00:18:56 -0500

    Finish getComputerChoice function
    - Add the getComputerChoice function to index.js
    - Returns rock, paper, or scissors randomly

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hi');
+function getComputerChoice() {
+    const randomNumber = Math.random();
+    if (randomNumber < 1 / 3) {
+        return 'rock';
+    } else if (randomNumber <= 0.67) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
