914b55beaacbb3044809a355fec6600e5236050a
Evan Ferreira <evanjfer@gmail.com>
2024-12-12 00:48:44 -0500

    Finish the game
    - Add playRound function to play 1 round against
    computer
    - Add playGame which uses playRound function 5 times
    to play 5 games to determine winner

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const randomNumber = Math.random();
     if (randomNumber < 1 / 3) {
@@ -11,7 +14,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice;
-    humanChoice = prompt('Enter rock, paper, or scissors: ');
+    humanChoice = prompt('Enter rock, paper, or scissors: ').toLowerCase();
     while (
         humanChoice !== 'rock' &&
         humanChoice !== 'paper' &&
@@ -22,4 +25,40 @@ function getHumanChoice() {
         );
     }
     return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie! Both of you selected " + humanChoice + '.');
+    } else if (
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        humanScore++;
+        console.log(
+            'You win! ' + humanChoice + ' beats ' + computerChoice + '.'
+        );
+    } else {
+        computerScore++;
+        console.log(
+            'You lose! ' + computerChoice + ' beats ' + humanChoice + '.'
+        );
+    }
+}
+
+function playGame() {
+    let humanChoice;
+    let computerChoice;
+    for (let i = 0; i < 5; i++) {
+        playRound(
+            (humanChoice = getHumanChoice()),
+            (computerChoice = getComputerChoice())
+        );
+    }
+    if (humanScore > computerScore) {
+        console.log('Congrats! You won the game! :)');
+    } else {
+        console.log('L + ratio, loser. :(');
+    }
 }
