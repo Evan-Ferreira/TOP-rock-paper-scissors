63ff7b35a3af7cf9266d895671d18a0e80903fd1
Evan Ferreira <evanjfer@gmail.com>
2024-12-13 18:36:19 -0500

    Finish the javascript logic for rps
    - Finish the javascript asked for as specified by
    TOP

--- index.js
+++ index.js
@@ -28,21 +28,46 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+    const pscore = document.querySelector('#pscore');
+    const cscore = document.querySelector('#cscore');
+    const ptext = pscore.textContent;
+    const ctext = cscore.textContent;
+    const h4 = document.querySelector('h4');
     if (humanChoice === computerChoice) {
-        console.log("It's a tie! Both of you selected " + humanChoice + '.');
+        h4.textContent =
+            "It's a tie! Both of you selected " + humanChoice + '.';
     } else if (
         (humanChoice === 'rock' && computerChoice === 'scissors') ||
         (humanChoice === 'paper' && computerChoice === 'rock') ||
         (humanChoice === 'scissors' && computerChoice === 'paper')
     ) {
         humanScore++;
-        console.log(
-            'You win! ' + humanChoice + ' beats ' + computerChoice + '.'
-        );
+        h4.textContent =
+            'You win! ' + humanChoice + ' beats ' + computerChoice + '.';
+        pscore.textContent = String(Number(ptext) + 1);
     } else {
         computerScore++;
-        console.log(
-            'You lose! ' + computerChoice + ' beats ' + humanChoice + '.'
-        );
+        h4.textContent =
+            'You lose! ' + computerChoice + ' beats ' + humanChoice + '.';
+        cscore.textContent = String(Number(ctext) + 1);
     }
 }
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+rock.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    playRound('rock', computerChoice);
+});
+
+paper.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    playRound('paper', computerChoice);
+});
+
+scissors.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    playRound('scissors', computerChoice);
+});
